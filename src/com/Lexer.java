61ddb0e189ec33f329ab05d7aaec25ea47a02eef
Faraday <faradaytrs@gmail.com>
2014-10-02 18:57:12 +0400

    Initial commit

---
+++ src/com/Lexer.java
@@ -0,0 +1,8 @@
+package com;
+
+/**
+ * Created by farad_000 on 02.10.2014.
+ */
+public class Lexer {
+
+}
---
+++ src/com/Translator.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * Created by farad_000 on 02.10.2014.
+ */
+public class Translator {
+
+	public static void main(String[] args) {
+
+		
+
+	}
+
+}
